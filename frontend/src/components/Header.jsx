52497cb654cf2c8480658ea5d6c2d5820a66fde9
Alexandra Jagonos <jagonosalexandra@gmail.com>
2026-02-15 00:02:17 +0800

    Update header image

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,15 +3,15 @@ import { assets } from '../assets/assets'
 
 const Header = ({heading, description}) => {
   return (
-    <div className='bg-primary text-center flex flex-col items-center justify-center min-h-142 relative isolate overflow-hidden p-6'>
-        <div className='flex flex-col gap-4 relative z-10'>
+    <div className='bg-primary text-center flex flex-col gap-2 pt-24'>
+        <div className='flex flex-col gap-4'>
             <h1 className='text-4xl md:text-6xl font-bold text-secondary'>{heading}</h1>
             <p className='text-gray-600'>{description}</p>
         </div>
         <img 
           src={assets.header} 
           alt='' 
-          className='absolute w-full -z-10' 
+          className='w-full' 
         />
     </div>
   )
